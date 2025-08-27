faf4e35e0556388b3467ab83d7be463dd68f3c41
Oops-infosolution34 <oopsmain@oopsinfosolution.com>
2025-08-27 16:01:25 +0530

    Add files via upload
    Test the server and views, All task in sequences

--- server.js
+++ server.js
@@ -4,26 +4,60 @@
 const path = require('path');
 const express = require('express');  // will create routes
 const ejs = require('ejs'); // for view purpose
- 
+const bodyparser = require('body-parser');
+
 const app = express();
 
 console.log(__dirname);
 
 app.set('views',path.join(__dirname,'views'));
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+
+
 
 app.set('view engine', 'ejs');
 
-app.get('/add',(req, res) => {
-     res.render("add");
+app.get('/',(req, res) => {
+     res.render("login");
   });
-app.get('/mod',(req, res) => {
-     res.render("mods");
+app.post('/login',(req, res) => {
+
+    console.log("welcome to login")
+
+    var username = req.body.t1;
+    var password = req.body.t2;
+
+    if(username=="admin" && password == "12345")
+         res.redirect("admin");
+    else if(username="raman" && password=="1234")
+        res.redirect("customer");
+    else
+        res.redirect("invalid");
   });
-app.get('/login',(req, res) => {
-    res.render("login");
+app.get('/admin',(req, res) => {
+     res.render("admin");
+  });
+app.get('/customer',(req, res) => {
+     res.render("customer");
+  });
+
+
+app.get('/invalid',(req, res) => {
+    res.render("invalid");
  });
- app.get('/success',(req, res) => {
-    res.render("success");
+
+
+ app.get('/addproduct',(req, res) => {
+    res.render("addproduct");
  });
+
+
+ app.get('/viewproduct',(req, res) => {
+    res.render("viewproduct");
+ });
+
 //server listening
-app.listen(9000);
+app.listen(9000, () => {
+  console.log('Server is running at port 9000');
+});
